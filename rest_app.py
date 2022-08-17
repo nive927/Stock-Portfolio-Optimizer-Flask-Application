1ae43338f046a1db3476f073b3d278de35b4ca11
Nivedhitha D <nivedhithadm@gmail.com>
2022-08-17 15:54:51 +0530

    template front-end for two routes

--- rest_app.py
+++ rest_app.py
@@ -1,9 +1,8 @@
 from distutils.log import debug
-from flask import Flask, request, render_template, Response
+from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import optimizer
-import pandas as pd
 
 def create_app():
 	app = Flask(__name__)
@@ -13,30 +12,41 @@ app = create_app()
 api = Api(app)
 CORS(app)
 
-class portfolio(Resource):
-    def get(self):
-        #ticker_list = "AAPL TSLA"
-        data = dict()
-        if request.args.get("tickers") and len(request.args.get("tickers").split()) >= 2:
-            ticker_list = request.args.get("tickers")
-            app.logger.info(ticker_list.split())
-            minRisk, maxReturn = optimizer.optimize(ticker_list)
-            data["minRisk"] = minRisk
-            data["maxReturn"] = maxReturn
-        else:
-            data["Error"] = "No portfolio provided"
-        app.logger.info(data)
-        return data
+# class portfolio_optimizer(Resource):
+#     def get(self):
+#         #ticker_list = "AAPL TSLA"
+#         data = dict()
+#         if request.args.get("tickers") and len(request.args.get("tickers").split()) >= 2:
+#             ticker_list = request.args.get("tickers")
+#             app.logger.info(ticker_list.split())
+#             minRisk, maxReturn = optimizer.optimize(ticker_list)
+#             data["minRisk"] = minRisk
+#             data["maxReturn"] = maxReturn
+#         else:
+#             data["Error"] = "No portfolio provided"
+#         app.logger.info(data)
+#         return render_template("portfolio_optimizer.html", data=data)
 
-api.add_resource(portfolio, '/api/portfolio')
+# api.add_resource(portfolio_optimizer, "/portfolio_optimizer")
 
 @app.route("/")
 def index():
-    # return """<h2 style='color:red;background-color:blue'>Portfolio Optimizer Rest API using Flask</h2>
-    # <p>Pass ticker symbols to tickers variable in URL string e.g. :</p>
-    # <a href='/api/portfolio?tickers=AAPL TSLA'>View Min Risk and Max Return Portfolios for FB and GOOGL</a>"""
-
     return render_template("index.html")
+
+@app.route("/portfolio_optimizer")
+def portfolio_optimizer():
+    #ticker_list = "AAPL TSLA"
+    data = dict()
+    if request.args.get("tickers") and len(request.args.get("tickers").split()) >= 2:
+        ticker_list = request.args.get("tickers")
+        app.logger.info(ticker_list.split())
+        minRisk, maxReturn = optimizer.optimize(ticker_list)
+        data["minRisk"] = minRisk
+        data["maxReturn"] = maxReturn
+    else:
+        data["Error"] = "No portfolio provided"
+    app.logger.info(data)
+    return render_template("portfolio_optimizer.html", data=data)
 
 if __name__ == '__main__':
     # app.run(host="0.0.0.0", port=8090)
